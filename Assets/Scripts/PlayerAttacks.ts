93bda7b485a54e7ac79ac46a6fcbbafc86b2f0e3
henryz2004 <henryz2004@gmail.com>
2024-09-29 03:40:39 -0400

    fixes projectile shooting

--- Assets/Scripts/PlayerAttacks.ts
+++ Assets/Scripts/PlayerAttacks.ts
@@ -1,4 +1,5 @@
 import { HandInteractor } from "SpectaclesInteractionKit/Core/HandInteractor/HandInteractor";
+import { HandInputData } from "SpectaclesInteractionKit/Providers/HandInputData/HandInputData";
 import { SIK } from "SpectaclesInteractionKit/SIK"
 
 @component
@@ -19,78 +20,81 @@ export class PlayerAttacks extends BaseScriptComponent {
 
     attack_timer = this.attack_cooldown;
 
+    handInputData: HandInputData = SIK.HandInputData;
+
     projectile_list: SceneObject[] = [];
 
     onAwake() {
         this.createEvent('UpdateEvent').bind(this.onUpdate.bind(this))
 
-    this.gestureModule
-        .getTargetingDataEvent(GestureModule.HandType.Right)
-        .add((targetArgs: TargetingDataArgs) => {
+        this.gestureModule
+            .getTargetingDataEvent(GestureModule.HandType.Right)
+            .add((targetArgs: TargetingDataArgs) => {
 
-        
-        if(this.attack_timer > 0){
-            // print('ON COOLDOWN')
-            
-            return
-        }
+                // if (this.handInputData.getHand('right').=== false) {
+                //     return;
+                // }
 
-        this.attack_timer = this.attack_cooldown;
-        // print('SHOT FIRED')
+                
+                if(this.attack_timer > 0 || !targetArgs.isValid){
+                    // print('ON COOLDOWN')
+                    
+                    return
+                }
+
+                this.attack_timer = this.attack_cooldown;
+                // print('SHOT FIRED')
 
 
-        let curr_proj = this.projectile_obj.instantiate(this.projectile_container)
-        this.projectile_list.push(curr_proj)
-        // print("PROJECTILE LIST LENGTH: " + this.projectile_list.length)
-        curr_proj.getTransform().setWorldPosition(targetArgs.rayOriginInWorld)
-        
-        //curr_proj.getTransform().setWorldPosition(new vec3(0,0,-300))
+                let curr_proj = this.projectile_obj.instantiate(this.projectile_container)
+                this.projectile_list.push(curr_proj)
+                // print("PROJECTILE LIST LENGTH: " + this.projectile_list.length)
+                curr_proj.getTransform().setWorldPosition(targetArgs.rayOriginInWorld)
+                
+                //curr_proj.getTransform().setWorldPosition(new vec3(0,0,-300))
 
-        let rb = curr_proj.getComponent('Physics.BodyComponent');
+                let rb = curr_proj.getComponent('Physics.BodyComponent');
 
-        if(rb === null){
-            print('NO RIGIDBODY AHHH')
-        }
+                if(rb === null){
+                    print('NO RIGIDBODY AHHH')
+                }
 
-        //rb.velocity = targetArgs.rayDirectionInWorld.normalize().uniformScale(this.launch_velocity)
-        //let test: vec3 = new vec3(0,10000,0)
-        
-        
-        let dir : vec3 = targetArgs.rayDirectionInWorld
-        dir.y = 0.1;
-        rb.addForce(dir.normalize().uniformScale(this.launch_velocity), Physics.ForceMode.VelocityChange)
-        
-        
-        //rb.addForce(new vec3(0,1,-300), Physics.ForceMode.VelocityChange)
-        //rb.velocity = test
+                //rb.velocity = targetArgs.rayDirectionInWorld.normalize().uniformScale(this.launch_velocity)
+                //let test: vec3 = new vec3(0,10000,0)
+                
+                
+                let dir : vec3 = targetArgs.rayDirectionInWorld
+                dir.y = 0.1;
+                rb.addForce(dir.normalize().uniformScale(this.launch_velocity), Physics.ForceMode.VelocityChange)
+                
+                
+                //rb.addForce(new vec3(0,1,-300), Physics.ForceMode.VelocityChange)
+                //rb.velocity = test
 
-        
-        //print(rb.velocity)
-        
-        /*
-        let probe = Physics.createGlobalProbe();
-        probe.debugDrawEnabled = true
-        probe.filter.includeStatic = true;
-        probe.filter.includeDynamic = false;
+                
+                //print(rb.velocity)
+                
+                /*
+                let probe = Physics.createGlobalProbe();
+                probe.debugDrawEnabled = true
+                probe.filter.includeStatic = true;
+                probe.filter.includeDynamic = false;
 
-        let end = targetArgs.rayOriginInWorld.add(targetArgs.rayDirectionInWorld.uniformScale(100));
-        
-        probe.rayCast(targetArgs.rayOriginInWorld, end, function (hit){
-            if(hit === null){
-                print('SHOT MISSED')
-                return;
+                let end = targetArgs.rayOriginInWorld.add(targetArgs.rayDirectionInWorld.uniformScale(100));
+                
+                probe.rayCast(targetArgs.rayOriginInWorld, end, function (hit){
+                    if(hit === null){
+                        print('SHOT MISSED')
+                        return;
+                    }
+                    print('SHOT HIT')
+                    let thing_I_shot = hit.collider.getSceneObject()
+
+                    thing_I_shot.destroy()
+                })
+            */
             }
-            print('SHOT HIT')
-            let thing_I_shot = hit.collider.getSceneObject()
-
-            thing_I_shot.destroy()
-        })
-        */
-        });
-        
-        
-
-    
+        );
     }
 
     onUpdate(){
